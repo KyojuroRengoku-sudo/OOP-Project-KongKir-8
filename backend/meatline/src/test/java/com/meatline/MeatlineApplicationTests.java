1ae3cb12cc782a2b03014c1cf0a8f29cf88679be
JirehDominguez <23102230@usc.edu.ph>
2026-09-23 10:22:39 +0800

    Rename project from Meatline to KongKir and update related files; add Maven wrapper and configuration

---
+++ backend/meatline/src/test/java/com/meatline/MeatlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
